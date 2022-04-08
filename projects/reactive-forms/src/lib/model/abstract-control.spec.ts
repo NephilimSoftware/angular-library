4dd3ec63f4fbc3a415be5a90e675ecf56349f49c
Szymon Michalak <szymon.michalak@nephilim.software>
2022-04-08 13:40:13 +0200

    Adds currentValue function for AbstractControl

---
+++ projects/reactive-forms/src/lib/model/abstract-control.spec.ts
@@ -0,0 +1,23 @@
+import {FormControl} from '@angular/forms';
+import {currentValue} from './abstract-control';
+import {read} from '../observable/read';
+
+describe('AbstractControl', () => {
+  let abstractControl: FormControl;
+
+  beforeEach(() => {
+    abstractControl = new FormControl('init');
+  });
+
+  describe('currentValue', () => {
+    it('gets initial value', async () => {
+      expect(await read(currentValue(abstractControl))).toBe('init');
+    });
+
+    it('gets last value when several "setValue" was called', async () => {
+      abstractControl.setValue('first');
+      abstractControl.setValue('second');
+      expect(await read(currentValue(abstractControl))).toBe('second');
+    })
+  });
+});
